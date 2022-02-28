e3410f0b23e47599389143ad84e509eb03ff808f
Johnathan Weisner <johnathan_weisner@yahoo.com>
2022-02-28 01:16:19 -0500

    tears

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ const getDefaultWord = () => [
 const classNameByLetterState = {
   [LetterState.FOUND]: styles.letterInputFound,
   [LetterState.LOCATION_UNKNOWN]: styles.letterInputLocationUnknown,
-  [LetterState.NOT_FOUND]: styles.letterInputNotFound,
+  [LetterState.NOT_FOUND]: styles.letterInputNotFound, // meh
 };
 
 const getNextIndexes = (wordIndex, letterIndex) => {
